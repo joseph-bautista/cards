3bdd86cd9ff19dfe6bb61587ecd3a2383b4c20de
Joseph Bautista <joseph@xctuality.com>
2024-09-03 20:49:55 +0800

    create cards better solution

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,9 +1,18 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  def contains?(deck, hand) do
+    Enum.member?(deck, hand)
   end
 end
